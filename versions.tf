325b2b65b9a5cd07f479ed3bd4a5940bc3ecab59
Maxime Poullain <maxime.poullain@fr.clara.net>
2022-10-31 16:18:56 +0100

    AZ-829: update caf version constraint

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.29"
+      version = "~> 3.29"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
